a1979ef0aa2df24678b4b5dcd26f3477c5b81f3b
Mathieu Gagné <mathieu@motioneleven.com>
2013-03-28 14:47:03 -0400

    Add less-rails dependency instead of less

--- lib/fuelux-rails/engine.rb
+++ lib/fuelux-rails/engine.rb
@@ -2,8 +2,9 @@ require 'rails'
 
 module FueluxRails
   class Engine < ::Rails::Engine
-    initializer 'fuelux-rails.setup', 
-      :after => 'less-rails.after.load_config_initializers', 
+    require 'less-rails'
+    initializer 'fuelux-rails.setup',
+      :after => 'less-rails.after.load_config_initializers',
       :group => :all do |app|
       app.config.less.paths << File.join(config.root, 'vendor', 'toolkit')
       end
